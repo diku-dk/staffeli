44d1401411eef68e06395b04374dea0fd9471c05
oleks <oleks@oleks.info>
2017-01-22 18:13:12 +0100

    That line was too long

---
+++ staffeli/upload.py
@@ -0,0 +1,33 @@
+import json
+import os
+import os.path
+import requests
+
+
+def via_post(
+        api_base: str, url_relative: str, token: str, filepath: str) -> int:
+
+    url = api_base + url_relative
+    headers = {
+        'Authorization': 'Bearer ' + token
+    }
+
+    name = os.path.basename(filepath)
+    size = os.stat(filepath).st_size
+
+    params = {
+        'name': name,
+        'size': size
+    }
+
+    resp = requests.post(url, headers=headers, params=params).json()
+
+    upload_url = resp['upload_url']
+    params = resp['upload_params']
+    name = resp['file_param']
+
+    with open(filepath, "rb") as f:
+        resp = requests.post(
+            upload_url, data=params, files=[(name, f)])
+
+    return json.loads(resp.text)['id']
