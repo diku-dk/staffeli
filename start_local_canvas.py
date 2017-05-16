19141e11b16f9edb5f8749059bcba22e8754704e
oleks <oleks@oleks.info>
2017-05-16 13:16:25 +0200

    No need to pull first, just run

--- start_local_canvas.py
+++ start_local_canvas.py
@@ -5,10 +5,6 @@ import sys
 from typing import List
 
 subprocess.check_call([
-    "docker", "pull",
-    "lbjay/canvas-docker"])
-
-subprocess.check_call([
     "docker", "run",
     "--publish=3000:3000",
     "--interactive", "--tty", "--rm",
