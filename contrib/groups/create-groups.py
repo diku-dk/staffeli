01efcd7c5edea2d764a327f775eebfaed04efd48
Simon Shine <shreddedglory@gmail.com>
2017-09-29 15:42:10 +0200

    Rewrite contrib/groups/create-groups.py for safety. #34
    - Turn this into a library (`if __name__ == "__main__"`).
    - Script still assumes that groups were not created on Canvas. If they were from a previous run, comment out the call to create_groups_on_canvas(). I don't think it actually creates those groups a second time, but running takes a bit longer since it makes an API call for each group it creates. Trying to create groups that already exist and are partly or wholly filled is a bug, though.
    - Throw with an informative message when multiple directories match a KU ID. Reasons could be: 'subs/' given when 'subs/N/' expected.
    - Throw with an informative message when two group members both upload files. This could be handled more gracefully by creating a group directory and symlink both submissions into it, rather than symlink the group directory directly to one of the submissions.
    - Filter empty submission directories based on their content (.staffeli.yml, group.txt) rather than their file count. This fixes a bug where the 'silent' partner uploads a group.txt for good measure and this script thinks there are two submitters.

--- contrib/groups/create-groups.py
+++ contrib/groups/create-groups.py
@@ -7,56 +7,102 @@ import re
 import os
 import glob
 
-import staffeli.canvas as canvas
+import staffeli.canvas as canvaslib
 
 
-course = canvas.Course()
-can = canvas.Canvas()
+# Maps abc123 (or whatever before @ in KU email address) to Canvas User ID
+def get_user_id_mapping(canvas, course):
+    user_ids = {}
+    for s in canvas.all_students(course.id):
+        m = re.match('^(.+?)@', s['login_id'])
+        user_ids[m.group(1)] = s['id']
+    return user_ids
 
-# Mapping from abc123 (or whatever is before @ in the KU email address) to
-# user id.
-user_ids = {}
-for s in can.all_students(course.id):
-    m = re.match('^(.+?)@', s['login_id'])
-    user_ids[m.group(1)] = s['id']
+def create_groups_on_canvas(canvas, course, category_name, user_ids):
+    group_cats = canvas.group_categories(course.id)
+    group_cat = next(filter(lambda cat: cat['name'] == category_name, group_cats))
+    group_cat_id = group_cat['id']
 
+    canvas_groups = canvas.groups(group_cat_id)
+    debug("There are {} groups for category {} ({}) already: {}".format(
+        len(canvas_groups),
+        category_name,
+        group_cat_id,
+        ", ".join(map(lambda group: group['name'], canvas_groups))))
+
+    for group_users, i in zip(groups, range(len(groups))):
+        group_name = '{} {:03d}'.format(category_name, i)
+        group_user_ids = list(map(lambda abc123: user_ids[abc123], group_users))
+
+        try:
+            debug("Creating group '{}'. ".format(group_name), end='')
+            group = canvas.create_group(group_cat_id, group_name)
+            group_id = group[0]['id']
+
+            debug("Adding group members {} to group: {}".format(
+                ", ".join(group_users), str(group)))
+            canvas.add_group_members(group_id, group_user_ids)
+        except Exception as wat:
+            print("Failed.")
+            print(wat)
+
+
+# create('subs/N/', 'Assignment N Groups', 'subs/N/groups.txt')
 def create(subs_path, category_name, groups_file):
+    canvas = canvaslib.Canvas()
+    course = canvaslib.Course()
+    debug = print
+
+    # Get mapping from abc123 to Canvas User ID.
+    user_ids = get_user_id_mapping(canvas, course)
+
     # Extract groups.
     with open(groups_file) as f:
         contents = f.read().strip()
     groups = [line.split(' ') for line in contents.split('\n')]
 
     # Update Canvas.
-    group_cats = can.group_categories(course.id)
-    group_cat = next(filter(lambda cat: cat['name'] == category_name,
-                            group_cats))
-    group_cat_id = group_cat['id']
-
-    for users, i in zip(groups, range(len(groups))):
-        group_name = '{} {:03d}'.format(category_name, i)
-        users = list(map(lambda abc123: user_ids[abc123], users))
-        group = can.create_group(group_cat_id, group_name)
-        group_id = group[0]['id']
-        can.add_group_members(group_id, users)
+    create_groups_on_canvas(canvas, course, category_name, user_ids)
 
     # Update locally.
     groupsubs_path = subs_path.replace('subs/', 'groupsubs/')
     os.makedirs(groupsubs_path, exist_ok=True)
-    for users, i in zip(groups, range(len(groups))):
-        groupsub_path = os.path.join(groupsubs_path, '{:03d}'.format(i))
-        users_submitted = []
-        for abc123 in users:
+    for group_members, group_number in zip(groups, range(len(groups))):
+        groupsub_path = os.path.join(groupsubs_path, '{:03d}'.format(group_number))
+        group_members_submitted = []
+        total_count = 0
+        for abc123 in group_members:
             pattern = os.path.join(subs_path, abc123) + '_*'
             subpaths = glob.glob(pattern)
-            assert len(subpaths) == 1
-            subpath = subpaths[0]
-            if len(os.listdir(subpath)) > 1:
-                # Ignore .staffeli.yml.
-                users_submitted.append(subpath)
-        assert len(users_submitted) == 1
-        submitting_user_path = users_submitted[0]
-        os.symlink(os.path.join('..', '..', submitting_user_path), groupsub_path)
+
+            # There should be 0 or 1 directories that match.
+            # 0 if, for some reason, a KU ID specified in a group.txt doesn't
+            #   have a submission. (This can be for other reasons than that it
+            #   is an invalid KU ID, or that that KU ID isn't subscribed to the
+            #   course. Example: cbh239 is on Advanced Programming 2017 and was
+            #   mentioned in a group.txt, they didn't submit anything
+            #   themselves for assignment 1, but they don't have an empty
+            #   directory like other students.
+            if len(subpaths) > 1:
+                raise Exception('Wait! Two submissions match {}: {}'.format(
+                    abc123, ", ".join(subpaths)))
+
+            elif len(subpaths) == 1:
+                subpath = subpaths[0]
+                ignore_files = [".staffeli.yml", "group.txt"]
+                files = [ f for f in os.listdir(subpath) if f not in ignore_files ]
+                if len(files) > 0:
+                    group_members_submitted.append(subpath)
+
+        if len(group_members_submitted) != 1:
+            raise Exception('Wait! Multiple submissions from {}: {}'.format(
+                ", ".join(group_members),
+                ", ".join(group_members_submitted)))
+
+        submitter_path = group_members_submitted[0]
+        os.symlink(os.path.join('..', '..', submitter_path), groupsub_path)
 
     return 0
 
-sys.exit(create(*sys.argv[1:]))
+if __name__ == "__main__":
+    sys.exit(create(*sys.argv[1:]))
